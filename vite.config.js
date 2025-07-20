8a0a4cd3be98ec735cae8c95ee533a85c14de159
Moch Adriq <mochfadillah1208@gmail.com>
2025-07-20 17:39:24 +0700

    fix: Mengupdate path server untuk Vercel

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,12 @@
+// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    // <-- TAMBAHKAN BAGIAN INI
     proxy: {
-      "/api": "http://localhost:3001",
+      "/api": "http://localhost:3001", // Pastikan ini adalah port backend lokal Anda
     },
   },
 });
