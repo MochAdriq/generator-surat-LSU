1bf85a1cdd566da1cb692bc9a60525672823d395
Moch Adriq <mochfadillah1208@gmail.com>
2025-07-21 15:54:07 +0700

    fix: Mengupdate path server untuk Vercel

--- api/index.js
+++ api/index.js
@@ -50,7 +50,7 @@ app.use((req, res, next) => {
   next();
 });
 
-app.post("/generate-inpassing-package", (req, res) => {
+app.post("/api/generate-inpassing-package", (req, res) => {
   try {
     const formData = req.body;
     const suratPengantarBuffer = generateDoc(
@@ -94,7 +94,7 @@ app.post("/generate-inpassing-package", (req, res) => {
   }
 });
 
-app.post("/generate-jad-package", (req, res) => {
+app.post("/api/generate-jad-package", (req, res) => {
   try {
     let formData = req.body;
 
